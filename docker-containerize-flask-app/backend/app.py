610970ef2ce137f4245342b4f9b5b01e8f8066d5
Aayushsingh2001 <as6994770@gmail.com>
2025-04-15 17:26:08 +0530

    Flask App containerize

---
+++ docker-containerize-flask-app/backend/app.py
@@ -0,0 +1,6 @@
+# Main server code
+from routes import app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+    
---
+++ docker-containerize-flask-app/backend/config.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    'host':'mysql', # Service name of your db
+    'user':'root',
+    'password':'9118151707',
+    'database':'mysample'
+}
